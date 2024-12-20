1cbb9cb79485dda22202bb30e9363891350f7b6b
Jerald Tonmoy Dias <tonmoyjareld@gmail.com>
2024-12-20 16:35:52 +0600

    customize header menu position

--- functions.php
+++ functions.php
@@ -32,6 +32,8 @@ add_action('wp_enqueue_scripts', 'procode_google_fonts');
 // theme function
 function procode_customizer_register($wp_customize)
 {
+
+  // ----------------- Header Section -----------------
   // add section
   $wp_customize->add_section('procode_header_section', array(
     'title' => __('Header Section', 'procode'),
@@ -50,6 +52,31 @@ function procode_customizer_register($wp_customize)
     'settings' => 'procode_header_logo',
     'priority' => 1
   )));
+
+  // ----------------- Menu Position Section -----------------
+  // add section
+  $wp_customize->add_section('procode_menu_option', array(
+    'title' => __('Menu Position Option', 'procode'),
+    'description' => sprintf(__('If you want to update settings, you can do it from here', 'procode')),
+    // 'priority' => 30
+  ));
+
+  $wp_customize->add_setting('procode_menu_position', array(
+    'default' => 'right_menu',
+    // 'type' => 'theme_mod'
+  ));
+
+  $wp_customize->add_control('procode_menu_position', array(
+    'label' => __('Menu Position', 'procode'),
+    'description' => 'Select the menu position',
+    'setting' => 'procode_menu_position',
+    'section' => 'procode_menu_option',
+    'type' => 'radio',
+    'choices' => array(
+      'left_menu' => 'Left Menu',
+      'right_menu' => 'Right Menu',
+      'center_menu' => 'Center Menu',
+    )));
 
 }
 
--- index.php
+++ index.php
@@ -1,34 +1,32 @@
+<?php
+/*
+* This template for displaying the header
+*/
+?>
 <!DOCTYPE html>
-<html <?php language_attributes(); ?> class="no-js">
-
+<html lang="<?php language_attributes(); ?>" class="no-js">
 <head>
-  <meta charset="<?php bloginfo('charset'); ?>">
+  <meta charset="<?php bloginfo('charset') ?>">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
   <?php wp_head(); ?>
 </head>
-
-<body <?php body_class() ?>>
-
-
-
-  <div id="header_area">
+<body <?php body_class(); ?>>
+  <div id="header_area" class="<?php echo get_theme_mod('procode_menu_position'); ?>">
     <div class="container">
       <div class="row">
         <div class="col-md-3">
-          <a href="#">
-            <img src="<?php echo get_theme_mod('procode_header_logo') ?>" alt="logo">
-          </a>
+          <a href=""><img src="<?php echo get_theme_mod('procode_header_logo'); ?>" alt=""></a>
         </div>
         <div class="col-md-9">
-        <?php wp_nav_menu(array('theme_location'=> 'main_menu','menu_id'=>'nav') ); ?>
+          <?php wp_nav_menu( array('theme_location' => 'main_menu', 'menu_id' => 'nav') ); ?>
         </div>
       </div>
     </div>
   </div>
 
 
+
   <?php wp_footer(); ?>
 </body>
-
 </html>
